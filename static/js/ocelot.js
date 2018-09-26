eb8ed5452d5e1e0c851dadd17a884c6159647af4
Dmitry <dmitry.zhidenkov@gmail.com>
2018-09-26 23:20:40 +0200

    fix: render artifacts

--- static/js/ocelot.js
+++ static/js/ocelot.js
@@ -62,9 +62,9 @@ function setBackground(r, g, b) {
 function set(x, y, value) {
   var px = x * charWidth;
   var py = y * charHeight;
-  context.clearRect(px, py, charWidth * value.length, charHeight + 1);
+  context.clearRect(px, py, charWidth * value.length, charHeight);
   context.fillStyle = backColor;
-  context.fillRect(px, py, charWidth * value.length, charHeight + 1);
+  context.fillRect(px, py, charWidth * value.length, charHeight);
   context.fillStyle = foreColor;
   context.fillText(value, px, py + fontOffset);
 }
@@ -74,16 +74,16 @@ function copy(x, y, width, height, xt, yt) {
   var py = y * charHeight;
   var tpx = (x + xt) * charWidth;
   var tpy = (y + yt) * charHeight;
-  var fragment = context.getImageData(px, py, width * charWidth, height * charHeight + 1);
+  var fragment = context.getImageData(px, py, width * charWidth, height * charHeight);
   context.putImageData(fragment, tpx, tpy);
 }
 
 function fill(x, y, width, height, value) {
   var px = x * charWidth;
   var py = y * charHeight;
-  context.clearRect(px, py, width * charWidth, height * charHeight + 1);
+  context.clearRect(px, py, width * charWidth, height * charHeight);
   context.fillStyle = backColor;
-  context.fillRect(px, py, width * charWidth, height * charHeight + 1);
+  context.fillRect(px, py, width * charWidth, height * charHeight);
   context.fillStyle = foreColor;
   var line = value.charAt(0).repeat(width);
   for (var i = 0; i < height; i++) {
@@ -233,85 +233,22 @@ terminal.oncontextmenu = function(e) {
 }
 
 var codes = {
-  49: 0x02,
-  50: 0x03,
-  51: 0x04,
-  52: 0x05,
-  53: 0x06,
-  54: 0x07,
-  55: 0x08,
-  56: 0x09,
-  57: 0x0A,
-  48: 0x0B,
-  65: 0x1E,
-  66: 0x30,
-  67: 0x2E,
-  68: 0x20,
-  69: 0x12,
-  70: 0x21,
-  71: 0x22,
-  72: 0x23,
-  73: 0x17,
-  74: 0x24,
-  75: 0x25,
-  76: 0x26,
-  77: 0x32,
-  78: 0x31,
-  79: 0x18,
-  80: 0x19,
-  81: 0x10,
-  82: 0x13,
-  83: 0x1F,
-  84: 0x14,
-  85: 0x16,
-  86: 0x2F,
-  87: 0x11,
-  88: 0x2D,
-  89: 0x15,
-  90: 0x2C,
-  222: 0x28,
-  50: 0x91,
-  8: 0x0E,
-  220: 0x2B,
-  20: 0x3A,
-  59: 0x92,
-  188: 0x33,
-  13: 0x1C,
-  61: 0x0D,
-  192: 0x29,
-  219: 0x1A,
-  17: 0x1D,
-  18: 0x38,
-  16: 0x2A,
-  173: 0x0C,
-  144: 0x45,
-  190: 0x34,
-  221: 0x1B,
-  191: 0x35,
-  32: 0x39,
-  9: 0x0F,
-  38: 0xC8,
-  40: 0xD0,
-  37: 0xCB,
-  39: 0xCD,
-  36: 0xC7,
-  35: 0xCF,
-  33: 0xC9,
-  34: 0xD1,
-  45: 0xD2,
-  46: 0xD3,
-  112: 0x3B,
-  113: 0x3C,
-  114: 0x3D,
-  115: 0x3E,
-  116: 0x3F,
-  117: 0x40,
-  118: 0x41,
-  119: 0x42,
-  120: 0x43,
-  121: 0x44,
-  122: 0x57,
-  123: 0x58
+  49: 0x02, 50: 0x03, 51: 0x04, 52: 0x05, 53: 0x06,
+  54: 0x07, 55: 0x08, 56: 0x09, 57: 0x0A, 48: 0x0B,
+  65: 0x1E, 66: 0x30, 67: 0x2E, 68: 0x20, 69: 0x12,
+  70: 0x21, 71: 0x22, 72: 0x23, 73: 0x17, 74: 0x24,
+  75: 0x25, 76: 0x26, 77: 0x32, 78: 0x31, 79: 0x18,
+  80: 0x19, 81: 0x10, 82: 0x13, 83: 0x1F, 84: 0x14,
+  85: 0x16, 86: 0x2F, 87: 0x11, 88: 0x2D, 89: 0x15,
+  90: 0x2C, 222: 0x28, 50: 0x91, 8: 0x0E, 220: 0x2B,
+  20: 0x3A, 59: 0x92, 188: 0x33, 13: 0x1C, 61: 0x0D,
+  192: 0x29, 219: 0x1A, 17: 0x1D, 18: 0x38, 16: 0x2A,
+  173: 0x0C, 144: 0x45, 190: 0x34, 221: 0x1B, 191: 0x35,
+  32: 0x39, 9: 0x0F, 38: 0xC8, 40: 0xD0, 37: 0xCB,
+  39: 0xCD, 36: 0xC7, 35: 0xCF, 33: 0xC9, 34: 0xD1,
+  45: 0xD2, 46: 0xD3, 112: 0x3B, 113: 0x3C, 114: 0x3D,
+  115: 0x3E, 116: 0x3F, 117: 0x40, 118: 0x41, 119: 0x42,
+  120: 0x43, 121: 0x44, 122: 0x57, 123: 0x58
 }
 
 document.onkeydown = function (e) {
