5d7c3a802a6b9c789d6fd7a761c136b322763eb6
Dmitry <dmitry.zhidenkov@gmail.com>
2019-09-03 11:50:14 +0200

    fake login error

--- src/js/ui/login.js
+++ src/js/ui/login.js
@@ -1,12 +1,14 @@
-export default {
-  view: function() {
+import { flash } from "../util/helpers.js";
+
+export default class Login {
+  view(vnode) {
     return m(".panel", { id: "login-form" }, [
       m("img", { class: "logo noselect", src: "images/logo.png", ondragstart: function() { return false; } }),
       m(".header", "Username"),
       m("input"),
       m(".header", "Password"),
       m("input"),
-      m("button", "Enter")
+      m("button", { onclick: function() { flash(vnode.dom, false, true); } }, "Enter")
     ])
   }
 }
--- src/js/util/helpers.js
+++ src/js/util/helpers.js
@@ -1,6 +1,7 @@
 // util methods
 // --------------------------------------------------------------------------------- //
 
+/** Returns an array with RGB values corresponding to a single value of 0xRRGGBB format */
 export function numberToColour(number) {
   const r = (number & 0xff0000) >> 16;
   const g = (number & 0x00ff00) >> 8;
@@ -8,15 +9,19 @@ export function numberToColour(number) {
   return [r, g, b];
 }
 
+/** Calculates a relative color lightness */
 export function lightness(r, g, b) {
   let max = Math.max(r, g, b);
   let min = Math.min(r, g, b);
-
   return (max + min) / 2;
 }
 
 const FANCY_ALPHA_TRESHOLD = 80;
 
+/**
+ * Calculate alpha channel value for the given color.
+ * Usually the bright colors will be opaque, and dark colors will be transparent.
+ */
 export function fancyAlpha(r, g, b) {
   let l = lightness(r, g, b);
 
@@ -26,6 +31,19 @@ export function fancyAlpha(r, g, b) {
     return 0.8 + (l / FANCY_ALPHA_TRESHOLD) * 0.2;
 }
 
+/** Check if Ocelot is running in mobile environment */
 export function isMobile() {
   return typeof window.orientation !== 'undefined';
+}
+
+/**
+ * Makes an element flash with red color for a second.
+ * Set `fore` to true to flash text, and `back` to flash background.
+ */
+export function flash(element, fore, back) {
+  element.classList.remove("warning-f");
+  element.classList.remove("warning-b");
+  void element.offsetWidth; // black magic - triggering element reflow
+  if (fore) element.classList.add("warning-f");
+  if (back) element.classList.add("warning-b");
 }
