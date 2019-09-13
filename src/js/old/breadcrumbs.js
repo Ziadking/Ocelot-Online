c3c93411f48f34f9d311a223c6ba07685bd45c14
Dmitry <dmitry.zhidenkov@gmail.com>
2019-09-13 15:59:02 +0200

    remove old views, optimize style sheets

--- src/js/old/breadcrumbs.js
+++
@@ -1,9 +0,0 @@
-import { state } from "../state.js"
-
-export default {
-  view: function() {
-    return state.breadcrumbs.map(function(crumb) {
-      return m(m.route.Link, { class: "item-breadcrumbs", href: crumb.url }, crumb.text);
-    });
-  }
-}
--- src/js/ui/workspace-list.js
+++ src/js/ui/workspace-list.js
@@ -3,12 +3,14 @@ import { state } from "../state.js"
 export default {
   view: function () {
     var array = state.workspaces.map(function(workspace) {
-      return m(".item .panel",
+      return m(".workspace-item",
         { onclick: function() { m.route.set('/workspace/:id', { id: workspace.id }) } },
-        [ workspace.name, m(".item-title", workspace.subtitle) ]
+        [ m(".workspace-item-title", workspace.name), m(".workspace-item-subtitle", workspace.subtitle) ]
       );
     });
-    if (state.loggedIn) array.push(m(".item.create .panel", ["+++", m(".item-title", "create new workspace")]));
+    if (state.loggedIn) array.push(m(".item.create", [
+      m(".workspace-item-title", "+++"), m(".workspace-item-subtitle", "create new workspace")
+    ]));
     return array;
   },
 }
