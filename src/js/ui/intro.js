4b76b73860ee72435b3614b71203765e438ac4e9
Dmitry <dmitry.zhidenkov@gmail.com>
2019-09-13 11:18:37 +0200

    make intro more readable, add hyperlinks

--- src/js/ui/intro.js
+++ src/js/ui/intro.js
@@ -6,9 +6,19 @@ export default class Intro {
       m("p", [
         "A free collaborative workspace is available for experimenting.",
         m("br"),
-        "And, of course, you can register an account and manage your own workspaces for something more serious."
+        "And, of course, you can ",
+        m(m.route.Link, { href: "/register" }, "register"),
+        " an account and manage your own ",
+        m(m.route.Link, { href: "/workspaces" }, "workspaces"),
+        " for something more serious."
       ]),
-      m("p", "Please report anything that bothers you to the issue tracker, or just to me in IRC."),
+      m("p", [
+        "Please report anything that bothers you to the ",
+        m("a", { href: "https://gitlab.com/cc-ru/ocelot/ocelot-online/issues" }, "issue tracker"),
+        ", or just to me in ",
+        m("a", { href: "https://webchat.esper.net/?join=cc.ru" }, "IRC"),
+        "."
+      ]),
       m("p", "I wish you a pleasant experience,"),
       m("p", "Totoro"),
     ]);
--- src/js/ui/layout.js
+++ src/js/ui/layout.js
@@ -3,10 +3,6 @@ import { BuildingMenu, NavigationMenu } from "./menu.js";
 var Layout = {
   view: function(vnode) {
     return [
-      m("img", {
-        id: "watermark", class: "centered noselect", src: "images/watermark.png",
-        ondragstart: function() { return false; }
-      }),
       m("div", { id: "container", class: "fullscreen" }, vnode.children),
       m(BuildingMenu),
       m(NavigationMenu),
